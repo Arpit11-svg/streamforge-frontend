15751b421cf4b571f1fba508c6abfca419ea6034
Arpit Choudhary <arpitchoudhary112006@gmail.com>
2026-07-27 00:16:33 +0530

    Add Button, Input, and Logo as reusable components

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logoImage from "../assets/streamforge_logo.png";
+
+function Logo({ width = "100px" }) {
+  return (
+    <div>
+      <img src={logoImage} alt="StreamForge Logo" style={{ width: width }} />
+    </div>
+  );
+}
+
+export default Logo;
